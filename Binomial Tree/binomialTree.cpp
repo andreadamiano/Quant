ce61f56844702cc46076ca342a1ada37972469ad
Andrea Damiano <decong.ad@gmail.com>
2025-04-26 22:14:05 +0200

    binomial tree

--- Binomial Tree/binomialTree.cpp
+++ Binomial Tree/binomialTree.cpp
@@ -5,7 +5,7 @@
 using namespace Eigen; 
 
 
-double binomial_american_option(const double& T, const int& N, const double& sigma, const double& S0, const double& r, const bool& is_call)
+double binomial_american_option(const double& T, const int& N, const double& sigma, const double& S0, const double& r, const bool& is_call, const double& K)
 {
     //discretize time 
     double dt = T /N; 
@@ -32,9 +32,34 @@ double binomial_american_option(const double& T, const int& N, const double& sig
         }
     }//result to a lower triangular matrix 
 
-    std::cout << S; 
+    //compute payoff at maturity for each leaf 
+    for (int j=0; j<N+1; j++)
+    {
+        if(is_call)
+            V(N, j) = std::max(S(N, j)-K, 0.0); 
 
-    return 0.0; 
+        else 
+            V(N, j) = std::max(K-S(N, j), 0.0); 
+    }
+
+    //backward induction of the option price , with early exercize 
+    for (int t = N-1; t>=0; --t)
+    {
+        for (int j=0; j<=t; ++j)
+        {
+            double CV = exp(-r*dt)*(V(t+1,j)*p + (1-p)*V(t+1,j+1)); //continuation value (if option is not exercised)
+
+            double EV = 0.0; 
+            if  (is_call)
+                EV = std::max(S(t,j)- K, 0.0); 
+            else    
+                EV = std::max(K - S(t,j), 0.0); 
+
+            V(t,j) = std::max(CV, EV); 
+        }
+    }
+
+    return V(0,0); //option price at t=0
 
 } 
 
@@ -47,8 +72,8 @@ int main ()
     double T = 1.0;      //time to expiration (1 year)
     double r = 0.05;     //risk-free interest rate (5%)
     double sigma = 0.2;  //volatility (20%)
-    int N = 10;         //number of time steps
+    int N = 4000;         //number of time steps
     bool is_call = true; //true for call option, false for put option
-    
-    double option_price = binomial_american_option(T, N, sigma, S0, r,  is_call);
+
+    std::cout << binomial_american_option(T, N, sigma, S0, r,  is_call, K);
 }
